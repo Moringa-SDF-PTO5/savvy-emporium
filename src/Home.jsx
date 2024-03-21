f463d4752ffcf05de6da4eb618350d4f16b91bd3
LameckKambi <lameck.kambi@moringaschool.com>
2024-03-22 01:10:49 +0300

    Added 'filter by category' buttons

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,6 +5,8 @@ import './Home.css'
 function Home() {
   const [products, setProducts] = useState([]);
   const [filteredProducts, setFilteredProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('All');
+
 
 
   useEffect(() => {
@@ -33,26 +35,36 @@ function Home() {
     .then(res => res.json())																									
     .then(data => setFilteredProducts(data))																									
     .catch(error => console.error(`Error fetching ${category} products:`, error));																									
-    }
-
+    }																									
+    setSelectedCategory(category); 																									
+    };
     
-
-
-
-  return (
-    <div className="product-grid">
-      {products.map(product => (
-        <div key={product.id} className="product-card">
-          <img src={product.image} alt={product.title} />
-          <h6>{product.title}</h6>
-            <p className="price">${product.price}</p>
-            <p className="category">{product.category}</p>
-        </div>
-      ))}
-    </div>
-  );
-}
-
-
-
-export default Home;
+    
+    return (																									
+    <div className="home-page">																									
+    <h4>Featured Products</h4>																									
+    <p>Selected Category: {selectedCategory}</p>																									
+    <div className="category-buttons">																									
+    <button onClick={() => filterProductsByCategory('All')} className={selectedCategory === 'All' ? 'selected' : ''}>All</button>																									
+    <button onClick={() => filterProductsByCategory('Electronics')} className={selectedCategory === 'Electronics' ? 'selected' : ''}>Electronics</button>																									
+    <button onClick={() => filterProductsByCategory('Jewelery')} className={selectedCategory === 'Jewelery' ? 'selected' : ''}>Jewelery</button>																									
+    <button onClick={() => filterProductsByCategory("Men's Clothing")} className={selectedCategory === "Men's Clothing" ? 'selected' : ''}>Men's Clothing</button>																									
+    <button onClick={() => filterProductsByCategory("Women's clothing")} className={selectedCategory === "Women's clothing" ? 'selected' : ''}>Women's Clothing</button>																									
+                                                      
+    </div>																									
+                                                      
+    <div className="product-grid">																									
+    {filteredProducts.map(product => (																									
+    <div key={product.id} className="product-card">																									
+    <img src={product.image} alt={product.title} />																									
+    <h6>{product.title}</h6>																									
+    <p className="price">${product.price}</p>																									
+    <p className="category">{product.category}</p>																									
+    </div>																									
+    ))}																									
+    </div>																									
+    </div>																									
+    );																									
+    }																									
+                                                      
+    export default Home;
