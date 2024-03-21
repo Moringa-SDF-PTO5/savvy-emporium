f35bf3943798b28e95106fd36b2ad65f93e373c1
LameckKambi <lameck.kambi@moringaschool.com>
2024-03-21 22:36:27 +0300

    Fetch API images and fixing errors

---
+++ src/Home.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
+
+function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        if (!response.ok) {
+          throw new Error('Failed to fetch products');
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+
+    fetchData(); 
+  }, []); 
+
+  return (
+    <div className="product-grid">
+      {products.map(product => (
+        <div key={product.id} className="product-card">
+          <img src={product.image} alt={product.title} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
