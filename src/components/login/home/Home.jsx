5fc4448bbafc378fe9d7e8e72ce97964af029399
LameckKambi <lameck.kambi@moringaschool.com>
2024-03-21 22:03:34 +0300

    Added a API fetch functionality

--- src/components/login/home/Home.jsx
+++ src/components/login/home/Home.jsx
@@ -1,12 +1,25 @@
-import React from "react";
-
-
+import React,{useEffect,useState} from "react";
 
 function Home (){
-
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        useEffect(() => {
+            const fetchData = async () => {
+              try {
+                const response = await fetch('https://fakestoreapi.com/products');
+                if (!response.ok) {
+                  throw new Error('Failed to fetch products');
+                }
+                const data = await response.json();
+                setProducts(data);
+                setFilteredProducts(data);
+              } catch (error) {
+                console.error('Error fetching products:', error);
+              }
+            };
     return (
         <div> </div>
     )
 }
-
+}
 export default Home;
