7c85ad90510cce107b4094ef78ce9d35f1168c5b
marynjoroge <wangarinjorogekenya@gmail.com>
2024-03-22 23:06:30 +0300

    Added profile route, Navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home'
+import Navbar from './components/structure/Navbar';
+import Footer from './components/structure/Footer';
+import Profile from './components/profile/Profile';
+
+
 
 function App() {
   return (
    <div> 
       <Router>
+          <Navbar />
       <Routes>  
           <Route path="/" element={<Home />} /> 
+          <Route path="/profile" element={<Profile/>} /> 
       </Routes>
-
+          <Footer/>
     </Router>
     </div>
   );
